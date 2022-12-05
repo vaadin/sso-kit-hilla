01901bd4808a144d52b8c0c17967c0c7da62fc85
Luciano Vernaschi <luciano@vaadin.com>
2022-12-05 16:28:53 +0100

    Return the OidcUser as User

--- sso-kit-hilla-demo/src/main/java/com/example/application/data/endpoint/UserEndpoint.java
+++ sso-kit-hilla-demo/src/main/java/com/example/application/data/endpoint/UserEndpoint.java
@@ -1,20 +1,30 @@
 package com.example.application.data.endpoint;
 
-import com.example.application.data.entity.User;
-import com.example.application.security.AuthenticatedUser;
-import com.vaadin.flow.server.auth.AnonymousAllowed;
-import dev.hilla.Endpoint;
 import java.util.Optional;
-import org.springframework.beans.factory.annotation.Autowired;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.oauth2.core.oidc.user.OidcUser;
+
+import com.example.application.data.entity.User;
+import com.vaadin.flow.server.auth.AnonymousAllowed;
+
+import dev.hilla.Endpoint;
 
 @Endpoint
 @AnonymousAllowed
 public class UserEndpoint {
 
-    @Autowired
-    private AuthenticatedUser authenticatedUser;
-
     public Optional<User> getAuthenticatedUser() {
-        return authenticatedUser.get();
+        return Optional.of(SecurityContextHolder.getContext())
+                .map(SecurityContext::getAuthentication)
+                .map(Authentication::getPrincipal)
+                .map(p -> (OidcUser) p)
+                .map(ou -> {
+                    User user = new User();
+                    user.setName(ou.getName());
+                    return user;
+                });
     }
 }
