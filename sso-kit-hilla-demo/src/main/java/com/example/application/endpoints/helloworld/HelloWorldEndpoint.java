2e43a042615d9cbcdb98a1608f630320b3ea7a8d
Luciano Vernaschi <luciano@vaadin.com>
2022-12-05 16:29:08 +0100

    Protect Hello World endpoint

--- sso-kit-hilla-demo/src/main/java/com/example/application/endpoints/helloworld/HelloWorldEndpoint.java
+++ sso-kit-hilla-demo/src/main/java/com/example/application/endpoints/helloworld/HelloWorldEndpoint.java
@@ -1,17 +1,17 @@
 package com.example.application.endpoints.helloworld;
 
+import com.example.application.data.endpoint.UserEndpoint;
 import com.vaadin.flow.server.auth.AnonymousAllowed;
 import dev.hilla.Endpoint;
 import dev.hilla.Nonnull;
 
 @Endpoint
-@AnonymousAllowed
 public class HelloWorldEndpoint {
 
     @Nonnull
     public String sayHello(@Nonnull String name) {
         if (name.isEmpty()) {
-            return "Hello stranger";
+            return "Hello " + new UserEndpoint().getAuthenticatedUser().get().getName();
         } else {
             return "Hello " + name;
         }
