5ac1f128b7955185abbded244af6a89c6205e404
Luciano Vernaschi <luciano@vaadin.com>
2022-12-05 16:28:38 +0100

    Tweak security configuration to remove Hilla default

--- sso-kit-hilla-demo/src/main/java/com/example/application/security/SecurityConfiguration.java
+++ sso-kit-hilla-demo/src/main/java/com/example/application/security/SecurityConfiguration.java
@@ -1,18 +1,15 @@
 package com.example.application.security;
 
-import com.vaadin.flow.spring.security.VaadinWebSecurity;
-import java.util.Base64;
-import javax.crypto.spec.SecretKeySpec;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.oauth2.jose.jws.JwsAlgorithms;
+import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+import com.vaadin.flow.spring.security.VaadinWebSecurity;
 
 @EnableWebSecurity
 @Configuration
@@ -23,8 +20,8 @@ public class SecurityConfiguration extends VaadinWebSecurity {
     // The secret is stored in /config/secrets/application.properties by default.
     // Never commit the secret into version control; each environment should have
     // its own secret.
-    @Value("${com.example.application.auth.secret}")
-    private String authSecret;
+    // @Value("${com.example.application.auth.secret}")
+    // private String authSecret;
 
     @Bean
     public PasswordEncoder passwordEncoder() {
@@ -37,10 +34,17 @@ public class SecurityConfiguration extends VaadinWebSecurity {
         http.authorizeRequests().requestMatchers(new AntPathRequestMatcher("/images/*.png")).permitAll();
         super.configure(http);
 
-        http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
-        setLoginView(http, "/login", LOGOUT_URL);
-        setStatelessAuthentication(http, new SecretKeySpec(Base64.getDecoder().decode(authSecret), JwsAlgorithms.HS256),
-                "com.example.application");
+        // http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+        // setLoginView(http, "/login", LOGOUT_URL);
+        // setStatelessAuthentication(http, new SecretKeySpec(Base64.getDecoder().decode(authSecret), JwsAlgorithms.HS256),
+        //         "com.example.application");
+    }
+
+    @Bean
+    @Override
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.oauth2Login().loginPage("/oauth2/authorization/keycloak");
+        return http.build();
     }
 
 }
