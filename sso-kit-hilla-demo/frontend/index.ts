67875e2b65e984398cfd18041bd2152f3346cc81
Luciano Vernaschi <luciano@vaadin.com>
2022-12-22 12:22:37 +0100

    Improve TS typing

--- sso-kit-hilla-demo/frontend/index.ts
+++ sso-kit-hilla-demo/frontend/index.ts
@@ -1,8 +1,16 @@
 import { Router } from '@vaadin/router';
+import ClientParameters from './generated/dev/hilla/sso/endpoint/ClientParameters';
 import { routes } from './routes';
 import { appStore } from './stores/app-store';
 
 export const router = new Router(document.querySelector('#outlet'));
+
+// Creates a type for the global Hilla object which is defined in index.html
+declare global {
+  const Hilla: {
+    SSO: ClientParameters;
+  };
+}
 
 appStore.fetchUserInfo().finally(() => {
   // Ensure router access checks are not done before we know if we are logged in
