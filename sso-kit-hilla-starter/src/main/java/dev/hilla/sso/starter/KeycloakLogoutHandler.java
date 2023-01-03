2b0a03d79eacb101f23463dfad9e1fd1510ff30f
Luciano Vernaschi <luciano@vaadin.com>
2023-01-03 14:36:35 +0100

    Use Hilla 1.3

--- sso-kit-hilla-starter/src/main/java/dev/hilla/sso/starter/KeycloakLogoutHandler.java
+++ sso-kit-hilla-starter/src/main/java/dev/hilla/sso/starter/KeycloakLogoutHandler.java
@@ -9,6 +9,9 @@
  */
 package dev.hilla.sso.starter;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
@@ -18,9 +21,6 @@ import org.springframework.security.web.authentication.logout.LogoutHandler;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.util.UriComponentsBuilder;
-
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
 
 @Component
 public class KeycloakLogoutHandler implements LogoutHandler {
