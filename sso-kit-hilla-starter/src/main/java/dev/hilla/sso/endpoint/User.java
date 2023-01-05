70fcfab1a994ce5f5af390488ad9e5314377f2a1
Luciano Vernaschi <cromoteca@users.noreply.github.com>
2023-01-05 11:40:38 +0100

    feat: add license headers to Java files

--- sso-kit-hilla-starter/src/main/java/dev/hilla/sso/endpoint/User.java
+++ sso-kit-hilla-starter/src/main/java/dev/hilla/sso/endpoint/User.java
@@ -1,3 +1,12 @@
+/*-
+ * Copyright (C) 2022-2023 Vaadin Ltd
+ *
+ * This program is available under Vaadin Commercial License and Service Terms.
+ *
+ *
+ * See <https://vaadin.com/commercial-license-and-service-terms> for the full
+ * license.
+ */
 package dev.hilla.sso.endpoint;
 
 import java.util.Set;
