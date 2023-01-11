6898bd2303ccadfc8ff0137bddd218461cb3fcaf
Luciano Vernaschi <cromoteca@users.noreply.github.com>
2023-01-11 13:20:54 +0100

    feat: add support for Okta (#36)

--- sso-kit-hilla-starter/src/main/java/dev/hilla/sso/endpoint/AuthEndpoint.java
+++ sso-kit-hilla-starter/src/main/java/dev/hilla/sso/endpoint/AuthEndpoint.java
@@ -9,17 +9,29 @@
  */
 package dev.hilla.sso.endpoint;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
+import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
+import org.springframework.security.oauth2.client.registration.InMemoryClientRegistrationRepository;
 import org.springframework.security.oauth2.core.oidc.user.OidcUser;
+import org.springframework.security.web.util.UrlUtils;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+import org.springframework.web.util.UriComponentsBuilder;
 
 import com.vaadin.flow.server.auth.AnonymousAllowed;
 
 import dev.hilla.Endpoint;
+import dev.hilla.Nonnull;
+import dev.hilla.sso.starter.SingleSignOnProperties;
+import jakarta.servlet.http.HttpServletRequest;
 
 @Endpoint
 @AnonymousAllowed
@@ -27,17 +39,15 @@ public class AuthEndpoint {
     private static final String ROLE_PREFIX = "ROLE_";
     private static final int ROLE_PREFIX_LENGTH = ROLE_PREFIX.length();
 
-    private final ClientParameters clientParameters;
+    private final ClientRegistrationRepository clientRegistrationRepository;
 
-    public AuthEndpoint(ClientParameters clientParameters) {
-        this.clientParameters = clientParameters;
-    }
+    private final SingleSignOnProperties properties;
 
-    // This method is not really useful since the configuration is already
-    // available on the client. It exists to let Hilla translate
-    // ClientParameters to TypeScript and get code completion and type safety.
-    public ClientParameters getClientParameters() {
-        return clientParameters;
+    public AuthEndpoint(
+            ClientRegistrationRepository clientRegistrationRepository,
+            SingleSignOnProperties properties) {
+        this.clientRegistrationRepository = clientRegistrationRepository;
+        this.properties = properties;
     }
 
     public Optional<User> getAuthenticatedUser() {
@@ -58,4 +68,84 @@ public class AuthEndpoint {
                     return user;
                 });
     }
+
+    /**
+     * Returns the URL to call to perform a logout from the OAuth2 provider.
+     *
+     * @return the URL
+     */
+    public Optional<String> getLogoutUrl() {
+        var authentication = SecurityContextHolder.getContext()
+                .getAuthentication();
+
+        if (authentication instanceof OAuth2AuthenticationToken
+                && authentication.getPrincipal() instanceof OidcUser) {
+
+            var logoutRedirectRoute = properties.getLogoutRedirectRoute();
+            String logoutUri;
+
+            // the logout redirect route can contain a {baseUrl} placeholder
+            if (logoutRedirectRoute.contains("{baseUrl}")) {
+                logoutUri = getCurrentHttpRequest()
+                        .map(request -> UriComponentsBuilder
+                                .fromHttpUrl(
+                                        UrlUtils.buildFullRequestUrl(request))
+                                .replacePath(request.getContextPath())
+                                .replaceQuery(null).fragment(null).build()
+                                .toUriString())
+                        .map(uri -> logoutRedirectRoute.replace("{baseUrl}",
+                                uri))
+                        .orElse(logoutRedirectRoute);
+            } else {
+                logoutUri = logoutRedirectRoute;
+            }
+
+            // Build the logout URL according to the OpenID Connect
+            // specification
+            var ou = (OidcUser) authentication.getPrincipal();
+            var registrationId = ((OAuth2AuthenticationToken) authentication)
+                    .getAuthorizedClientRegistrationId();
+            var clientRegistration = clientRegistrationRepository
+                    .findByRegistrationId(registrationId);
+            var details = clientRegistration.getProviderDetails();
+            // The end_session_endpoint is buried in the provider metadata
+            var endSessionEndpoint = details.getConfigurationMetadata()
+                    .get("end_session_endpoint").toString();
+            var builder = UriComponentsBuilder.fromUriString(endSessionEndpoint)
+                    .queryParam("id_token_hint",
+                            ou.getIdToken().getTokenValue())
+                    .queryParam("post_logout_redirect_uri", logoutUri);
+            return Optional.of(builder.toUriString());
+        }
+
+        return Optional.empty();
+    }
+
+    /**
+     * Returns the ids of the registered OAuth2 clients.
+     *
+     * @return a list of registration ids
+     */
+    @Nonnull
+    public List<@Nonnull String> getRegisteredClients() {
+        return Optional.of(clientRegistrationRepository)
+                // By default, the client registration repository is an instance
+                // of InMemoryClientRegistrationRepository
+                .filter(InMemoryClientRegistrationRepository.class::isInstance)
+                .map(InMemoryClientRegistrationRepository.class::cast)
+                .map(repo -> {
+                    List<String> list = new ArrayList<>();
+                    repo.iterator().forEachRemaining(registration -> list
+                            .add(registration.getRegistrationId()));
+                    return list;
+                }).orElse(List.of());
+    }
+
+    private static Optional<HttpServletRequest> getCurrentHttpRequest() {
+        return Optional.ofNullable(RequestContextHolder.getRequestAttributes())
+                .filter(ServletRequestAttributes.class::isInstance)
+                .map(ServletRequestAttributes.class::cast)
+                .map(ServletRequestAttributes::getRequest);
+    }
+
 }
