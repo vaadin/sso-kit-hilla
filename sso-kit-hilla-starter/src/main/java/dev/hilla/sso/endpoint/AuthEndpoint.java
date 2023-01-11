b3e07259f8cb4ead88715563d568f0ed97547c12
Luciano Vernaschi <luciano@vaadin.com>
2023-01-11 14:25:46 +0100

    Implement back channel logout and demo dialog
    fix bc

--- sso-kit-hilla-starter/src/main/java/dev/hilla/sso/endpoint/AuthEndpoint.java
+++ sso-kit-hilla-starter/src/main/java/dev/hilla/sso/endpoint/AuthEndpoint.java
@@ -14,8 +14,6 @@ import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
-import javax.servlet.http.HttpServletRequest;
-
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -34,21 +32,37 @@ import dev.hilla.Endpoint;
 import dev.hilla.Nonnull;
 import dev.hilla.sso.starter.SingleSignOnProperties;
 
+import java.util.Objects;
+
+import javax.annotation.security.PermitAll;
+import javax.servlet.http.HttpServletRequest;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import dev.hilla.EndpointSubscription;
+import dev.hilla.sso.starter.bclogout.FluxHolder;
+
 @Endpoint
 @AnonymousAllowed
 public class AuthEndpoint {
     private static final String ROLE_PREFIX = "ROLE_";
     private static final int ROLE_PREFIX_LENGTH = ROLE_PREFIX.length();
+    private static final Logger LOGGER = LoggerFactory
+            .getLogger(AuthEndpoint.class);
 
     private final ClientRegistrationRepository clientRegistrationRepository;
 
     private final SingleSignOnProperties properties;
 
+    private final FluxHolder fluxHolder;
+
     public AuthEndpoint(
             ClientRegistrationRepository clientRegistrationRepository,
-            SingleSignOnProperties properties) {
+            SingleSignOnProperties properties, FluxHolder fluxHolder) {
         this.clientRegistrationRepository = clientRegistrationRepository;
         this.properties = properties;
+        this.fluxHolder = fluxHolder;
     }
 
     public Optional<User> getAuthenticatedUser() {
@@ -149,4 +163,20 @@ public class AuthEndpoint {
                 .map(ServletRequestAttributes::getRequest);
     }
 
+    @PermitAll
+    @Nonnull
+    public EndpointSubscription<@Nonnull String> backChannelLogout() {
+        LOGGER.debug("Client subscribed to back channel logout information");
+        var principal = SecurityContextHolder.getContext().getAuthentication()
+                .getPrincipal();
+
+        var flux = fluxHolder.getFlux().filter(p -> {
+            return Objects.equals(p, principal);
+        }).map(p -> "Your session has been terminated");
+
+        return EndpointSubscription.of(flux, () -> {
+            LOGGER.debug(
+                    "Client cancelled subscription to back channel logout information");
+        });
+    }
 }
--- sso-kit-hilla-starter/src/main/java/dev/hilla/sso/starter/SingleSignOnProperties.java
+++ sso-kit-hilla-starter/src/main/java/dev/hilla/sso/starter/SingleSignOnProperties.java
@@ -11,6 +11,8 @@ package dev.hilla.sso.starter;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
+import dev.hilla.sso.starter.bclogout.BackChannelLogoutFilter;
+
 /**
  * Definition of configuration properties for the SSO Kit Hilla starter.
  *
@@ -39,6 +41,23 @@ public class SingleSignOnProperties {
     static final String DEFAULT_LOGOUT_REDIRECT_ROUTE = "{baseUrl}";
 
     /**
+     * The default maximum number of concurrent sessions allowed per user: -1
+     * means any number of concurrent sessions is allowed.
+     */
+    static final int DEFAULT_MAXIMUM_SESSIONS_PER_USER = -1;
+
+    /**
+     * The default Back-Channel Logout route. This should be the same as in the
+     * OIDC provider's configuration to be able to accept logout notices as
+     * described by the specification. It requires a URI variable to match the
+     * client registration-id: {@code registrationId}.
+     *
+     * @see https://openid.net/specs/openid-connect-backchannel-1_0.html
+     */
+    public static final String DEFAULT_BACKCHANNEL_LOGOUT_ROUTE = "/logout/back-channel/{"
+            + BackChannelLogoutFilter.REGISTRATION_ID_URI_VARIABLE_NAME + "}";
+
+    /**
      * Enables (or disables) auto-configuration.
      */
     private boolean autoConfigure = true;
@@ -52,6 +71,23 @@ public class SingleSignOnProperties {
      * The route to redirect to after successful logout.
      */
     private String logoutRedirectRoute = DEFAULT_LOGOUT_REDIRECT_ROUTE;
+
+    /**
+     * If set to {@code true} it enables support for Back-Channel logout.
+     */
+    private boolean backChannelLogout = false;
+
+    /**
+     * The route to match Back-Channel logout requests against. The default
+     * value is {@code /logout/back-channel/{registration-id}}.
+     */
+    private String backChannelLogoutRoute = DEFAULT_BACKCHANNEL_LOGOUT_ROUTE;
+
+    /**
+     * The number of maximum concurrent sessions allowed per user. The default
+     * value is -1 which means any number of concurrent sessions is allowed.
+     */
+    private int maximumConcurrentSessions = DEFAULT_MAXIMUM_SESSIONS_PER_USER;
 
     /**
      * Checks is auto-configuration of {@link SingleSignOnConfiguration} is
@@ -111,5 +147,63 @@ public class SingleSignOnProperties {
      */
     public void setLogoutRedirectRoute(String logoutRedirectRoute) {
         this.logoutRedirectRoute = logoutRedirectRoute;
+    }
+
+    /**
+     * Checks weather Back-Channel logout is enabled.
+     *
+     * @return {@code true} if Back-Channel logout is enabled, {@code false}
+     *         otherwise
+     */
+    public boolean isBackChannelLogout() {
+        return backChannelLogout;
+    }
+
+    /**
+     * Sets is Back-Channel logout is enabled.
+     *
+     * @param backChannelLogout
+     *            weather to enable or disable Back-Channel logout
+     */
+    public void setBackChannelLogout(boolean backChannelLogout) {
+        this.backChannelLogout = backChannelLogout;
+    }
+
+    /**
+     * Gets the Back-Channel Logout route.
+     *
+     * @return the Back-Channel Logout route
+     */
+    public String getBackChannelLogoutRoute() {
+        return backChannelLogoutRoute;
+    }
+
+    /**
+     * Sets the Back-Channel Logout route.
+     *
+     * @param backchannelLogoutRoute
+     *            the Back-Channel Logout route
+     */
+    public void setBackChannelLogoutRoute(String backchannelLogoutRoute) {
+        this.backChannelLogoutRoute = backchannelLogoutRoute;
+    }
+
+    /**
+     * Gets the maximum number of concurrent sessions per user.
+     *
+     * @return the maximum number of concurrent sessions
+     */
+    public int getMaximumConcurrentSessions() {
+        return maximumConcurrentSessions;
+    }
+
+    /**
+     * Sets maximum number of concurrent sessions per user.
+     *
+     * @param maximumConcurrentSessions
+     *            maximum number of concurrent sessions
+     */
+    public void setMaximumConcurrentSessions(int maximumConcurrentSessions) {
+        this.maximumConcurrentSessions = maximumConcurrentSessions;
     }
 }
